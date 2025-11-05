4bc6ec1481eacae4570bcaec1fbba90579f7531b
Scott Ernst <swernst@gmail.com>
2025-11-04 18:34:03 -0600

    MyPy Updates

--- pipper/__init__.py
+++ pipper/__init__.py
@@ -8,10 +8,10 @@ try:
 except _metadata.PackageNotFoundError:  # pragma: no-cover
     # If the package is not installed such that it has distribution metadata
     # fallback to loading the version from the pyproject.toml file.
-    _metadata = _toml.loads(
+    _package_metadata = _toml.loads(
         _pathlib.Path(__file__).parent.parent.joinpath("pyproject.toml").read_text()
     )
     try:
-        __version__ = _metadata["tool"]["poetry"]["version"]
+        __version__ = _package_metadata["tool"]["poetry"]["version"]
     except KeyError:
-        __version__ = _metadata["project"]["version"]
+        __version__ = _package_metadata["project"]["version"]
